5c5f8664f2f79c094f116d2a1d446b08572e4e3e
Noemi Leon <noemi.telematik213@gmail.com>
2022-08-24 18:26:30 -0500

    change path in server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // Web server for the compiled app.
 const path = require('path')
 const express = require('express') // Third Party library.
+const serveStatic = require('serve-static')
 
 const app = express()
 
-//here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
+let buildDir = path.join(__dirname, '/dist')
+app.use('/', serveStatic(buildDir))
+app.get(/.*/, function(req, res) {
+  let indexPath = path.join(__dirname, '/dist/index.html')
+  res.sendFile(indexPath)
 })
 
 process.env.PORT = process.env.PORT ? process.env.PORT : 3300
